08120f33cb37a2edc1029b0018a1afc8b716989a
Marcus Robinson <31769703+marcusjrob@users.noreply.github.com>
2020-02-20 20:14:54 -0500

    Update and rename RobotRecieveSignal( no sensors).ino to RobotSendandRecieveSignal.ino

--- SendSignal/RobotSendandRecieveSignal.ino
+++ SendSignal/RobotSendandRecieveSignal.ino
@@ -1,3 +1,14 @@
+#include <SharpIR.h>
+
+#define LS1 A0
+#define LS2 A1
+#define LS3 A2
+#define LS4 A3
+#define LS5 A4
+#define LS6 A5
+#define LS7 A6
+
+#define model 1080
 // If get wireless signal:
 // 11-001-000 move forward, i.e. moveForward()
 // 11-010-000 move backward, i.e. moveBackward()
@@ -7,13 +18,22 @@
 // 11-110-000 transmit sensor data, i.e. sendDistance()
 
 // Transmit distance sensor values once after every move, i.e. sendDistance()
-
+SharpIR L1(LS1, model);
+SharpIR L2(LS2, model);
+SharpIR L3(LS3, model);
+SharpIR L4(LS4, model);
+SharpIR L5(LS5, model);
+SharpIR L6(LS6, model);
+SharpIR L7(LS7, model);
 // All transmissions are 8-bit, starting with header
 const int headerLength = 8;
 bool header [headerLength] = {1,0,1,1,0,0,1,0}; 
 
 const int RxLength = 14;
 bool RxSignal [RxLength];
+
+const int TxLength = 56;
+bool TxSignal [TxLength];
 
 unsigned long checkoutTime = millis();
 unsigned long currentTime;
@@ -22,7 +42,7 @@ int period = 10;
 
 
 // *Parshva - Make pins for distance sensors here
-int frontRight(2), frontLeft(4), backRight(6), backLeft(8); // Wheels
+int frontRight(2),frontRightb(3), frontLeft(4),frontLeftB(5), backRight(6),backRightb(7), backLeft(8),backLeftb(9); // Wheels
 int Rx(11), Tx(12); // Antennas
 char pastMoves[20]; // Assuming a maximum of 20 moves stored
 int pastMoveCount = 0;
@@ -58,11 +78,28 @@ void loop()
       }
   }
 
-  if(currentTime > (checkoutTime + 30000)){
-    //Escape();
+  /*if(currentTime > (checkoutTime + 30000)){
+    Escape();
     sendDistance();
     checkoutTime = currentTime;
-   }
+   }*/
+
+   //Testing
+     while (Serial.available()){
+    char a = Serial.read(); // Read incoming character
+    
+    switch (a) { 
+      case ('F'):
+        sendDistance();
+        PrintTx();
+        break;
+      case ('B'):
+        
+        break;
+    }
+  }
+
+
 }
 
 // performAction - Reads the incoming message and performs the necessary action
@@ -170,17 +207,43 @@ void Escape(){
 
 //sendDistance - measure distance using sensor inputs
 void sendDistance(){
+  int dis[7]= {0,1,2,4,8,16,80};
+
+  /*
+  dis[0]= L1.distance();
+  dis[1]= L2.distance();
+  dis[2]= L3.distance();
+  dis[3]= L4.distance();
+  dis[4]= L5.distance();
+  dis[5]= L6.distance();
+  dis[6]= L7.distance();
+  */
+
+  int k = TxLength - 1;
+  for(int i=6; i >= 0; i--){
+    for(int j=0; j < 8; j++){
+      TxSignal[k] = dis[i]%2;
+      dis[i] = dis[i]/2;
+      k--;
+    }
+  }
  return;
 }
 
 // sendMessage - Send an array of bits
-void sendMessage(int *message, int messageLength){
-  for (int i = 0; i < messageLength; i++){
-    digitalWrite(Tx, message[i]);
+void sendMessage(){  
+  for(int i =0;i < headerLength; i++){
+    digitalWrite(Tx, header[i]);
     delay(period);
     }
-    digitalWrite(Tx, 0);
-    return;
+  
+  for (int i = headerLength; i < TxLength; i++){
+    digitalWrite(Tx, TxSignal[i]);
+    delay(period);
+    }
+    
+  digitalWrite(Tx, 0);
+  return;
 }
 
 //checkHeader - Compare recorded bits to the header
@@ -202,10 +265,24 @@ void appendSignal(bool RxNewBit){
   return;
 }
 
+//Decode the integer value of the bits after the header
 int RxInteger(){
   int decimal = 0;
   for(int i = headerLength; i < RxLength; i++){
     decimal = (decimal*2) + RxSignal[i];
     }
   return decimal;
+}
+
+//(Function For Testing) Print TxSignal
+void PrintTx(){
+  Serial.println("\nSignal:");
+  for(int i=0; i< 7; i++){
+    for(int j=0; j < 8; j++){
+      int index = (8*i)+j;
+      //Serial.print(index);
+      Serial.print(TxSignal[index]);
+      }
+    Serial.print(" ");
+  }
 }
